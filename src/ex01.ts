d245b40cb62825a088783b67fe2ba34bf697e580
Geilson A. Soares <96357353+geilsonsrz@users.noreply.github.com>
2022-07-01 00:25:02 -0300

    Manual file upload.
    Failed to commit the files directly from the terminal.

---
+++ src/ex01.ts
@@ -0,0 +1,115 @@
+/*Exercício 01 de TypeScript - Declarando tipos*/
+//Estação - SIMULAÇÃO DO DB
+const estacao:any[] = [];
+
+//Criação da nave
+function novaNave( name:string, pilot:string, crewLimit:number ){
+    const nave = { name, pilot, crewLimit, crew: [pilot], inMission:false }
+    estacao.push(nave)
+    alert(`Nave ${nave.name} foi registrada na Estação`)
+};
+
+//Localizar nave
+function acharNave( name:string ){
+
+    let nave: {
+        name:string,
+        pilot: string,
+        crew: string[],
+        crewLimit: number,
+        inMission: boolean
+    }
+    nave = estacao.find( nave => nave.name == name )
+    return nave
+};
+
+//Function Case 1
+function primeiroCaso(){
+    let nome = String(prompt("Nome da NAVE:")).toUpperCase()
+    let piloto = String(prompt("Nome do PILOTO:"))
+    let maxTripu = Number(prompt("Número máx de tripulantes:"))
+    novaNave(`${nome}`, `${piloto}`, maxTripu)
+};
+
+//Function Case 2
+function segundoCaso(){
+    let nome = String(prompt("Nome da NAVE:")).toUpperCase()
+    let nave = acharNave(nome)
+    if( nave.crew.length < nave.crewLimit ){
+        let novoTripulante = String(prompt("Novo tripulante:"))
+        nave.crew.push(novoTripulante)
+        alert(`Tripulante ${novoTripulante} embarcou.`)
+    }else{
+        alert(`Capacidade max atingida. Nao é possível embarcar.`)
+    }
+
+};
+
+//Function Case 3
+function terceiroCaso(){
+    let nome = String(prompt("Nome da NAVE")).toUpperCase()
+    let nave = acharNave(nome)
+    if( nave.crew.length < Math.floor(nave.crewLimit/3) ){
+        alert("Saída negada! Baixo número de tripulantes.")
+    }else if( nave.inMission ){
+        alert("Nave ja esta em uma missao!")
+    }else{
+        nave.inMission = true
+        alert("Nave enviada para missao!")
+    }
+};
+
+//Function Case 4
+function quartoCaso(){
+    let list:string = ("Listar todas as naves registradas\n")
+    estacao.forEach( nave => {
+        list += `
+        Nave: ${nave.name}
+        Piloto: ${nave.pilot}
+        Limite: ${nave.crewLimit}
+        Ocupação Atual: ${nave.crew.length}
+        Em missão: ${nave.inMission}\n`
+    });
+    alert(list)
+};
+
+
+//Gerenciamento da estação
+let q:number = 0 
+while(q==0){
+    
+    let p = Number(prompt(`
+    Menu:
+    1- Cadastrar nova nave;
+    2- Alterar tripulação da nave;
+    3- Enviar nave para uma missão;
+    4- Listar todas as naves registradas;
+    5- Sair.`))
+    
+    switch(p){
+
+        case 1:
+            primeiroCaso();
+            break
+            
+        case 2:
+            segundoCaso();
+            break
+
+        case 3:
+            terceiroCaso();
+            break
+
+        case 4:
+            quartoCaso();
+            break
+
+        case 5:
+            q=1
+            break
+        
+        default:
+            alert("Valor inválido!")
+            break
+    }
+};
---
+++ src/ex02.ts
@@ -0,0 +1,212 @@
+/*
+    EXERCÍCIO 2 - CRIANDO NOVOS TIPOS
+*/
+
+//Criação dos tipos
+//Situação
+type PlanetSituation = "Habitado" | "Habitável" | "Inabitável" | "Inexplorado"
+//Coordenadas
+type PlanetCoordinates = [ number, number, number, number ]
+//Planeta
+type Planet = {
+    name: string;
+    coordinates: PlanetCoordinates;
+    situation: PlanetSituation;
+    satellites: string[] 
+}
+
+//Criação do Array de Planetas - Simulador do DB
+//Uma array que recebe valores do tipo Planet.
+const planets: Planet[] = []
+
+//Função para adicionar um planeta
+function addPlanet(name:string, coordinates:PlanetCoordinates, situation:PlanetSituation){
+    //Recebe os parâmetros e cria um objeto e adiciona em planetas.
+    planets.push({
+        name,
+        coordinates,
+        situation,
+        satellites: []
+    })
+    alert(`O planeta ${name} foi adicionado.`)
+};
+
+//Função para procurar um planeta.
+function findPlanet(name:string){
+    //Localizando planeta
+    const planet = planets.find( planeta => planeta.name == name )
+    //?? serve para agregar um valor padrão.
+    //Retorna um planeta, caso contrário, retorna falso.
+    return planet ?? false
+};
+
+//Função para atualizar uma situação.
+function situationPlanet(situation:PlanetSituation, planet:Planet){
+    //Atualizando a situação conforme o parâmetro recebido.
+    planet.situation = situation
+    alert(`A situação do planeta ${planet.name} foi atualizada para ${situation}.`)
+};
+
+//Adicionar satélites ao planeta
+function addSatellite(satellite:string, planet:Planet){
+    //Adicionando o satélite conforme parâmetro
+    planet.satellites.push(satellite)
+    alert(`O satélite ${satellite} foi adicionado ao planeta ${planet.name}.`)
+};
+
+//Remover satélite do planeta
+//Utilizando um método de filtro, em vez de remover por id
+//O filter retorna uma array, no caso, retorna todos os nomes diferentes do parâmetro.
+function removeSatellite(satellite:string, planet:Planet){
+    planet.satellites = planet.satellites.filter( satelite => satelite !== satellite )
+    alert(`O satélite ${satellite} foi removido do planeta ${planet.name}.`)
+};
+
+//Função para validar situação do planeta
+function validSituation(){
+    //Opções das situações
+    let situation:PlanetSituation
+    //Controle do loop
+    let valid = true
+    while(valid){
+        let situationInput = prompt(`
+        Qual a situação do planete:
+        1 - Habitado
+        2 - Habitável
+        3 - Inabitável
+        4 - Inexplorado
+        `)
+        switch(situationInput){
+            case "1":
+                situation = "Habitado"
+                valid = false
+                break
+            case "2":
+                situation = "Habitável"
+                valid = false
+                break
+            case "3":
+                situation = "Inabitável"
+                valid = false
+                break
+            case "4":
+                situation = "Inexplorado"
+                valid = false
+                break
+            default:
+                alert("Digite o numero de uma situação válida.")
+                break
+        }
+    }
+    //Acusa erro mas tudo indica ser um bug da IDE
+    return situation;
+};
+
+//Função para validação do planeta
+function validPlanet(callbackfn: (planeta:Planet)=>void){
+    const planetName = String(prompt("Nome do planeta: "))
+    const planet = findPlanet(planetName)
+
+    if(planet){
+        callbackfn(planet)
+    }else{
+        alert("Planeta não localizado!")
+    }
+};
+
+//Primeira função - Registrar um novo planeta
+function primeiraF(){
+    let nome = String(prompt("Nome do planeta:"))
+    let coordenadaA = Number(prompt("Coordenada A:"))
+    let coordenadaB = Number(prompt("Coordenada B:"))
+    let coordenadaC = Number(prompt("Coordenada C:"))
+    let coordenadaD = Number(prompt("Coordenada D:"))
+    let situacao = validSituation()
+    addPlanet(nome, [coordenadaA, coordenadaB, coordenadaC, coordenadaD], situacao)
+};
+
+//Segunda função - Atualizar situação do planeta
+function segundaF(){
+    validPlanet(planeta => {
+        let situacao = validSituation()
+        situationPlanet(situacao,planeta)
+    })
+};
+
+//Terceira função - Adicionar um satélite ao planeta
+function terceiraF(){
+    validPlanet(planeta => {
+        let satelite = String(prompt("Nome do satélite:"))
+        addSatellite(satelite, planeta)
+    })
+};
+
+//Quarta função - Remover um satélite do planeta
+function quartaF(){
+    validPlanet(planeta => {
+        let satelite = String(prompt("Nome do satélite:"))
+        removeSatellite(satelite, planeta)
+    })
+};
+
+//Quinta função - Lista todos os planetas
+function quintaF(){
+    let list = "Planetas:\n"
+
+    //Agregando cada planeta ao texto.
+    planets.forEach( planeta => {
+        //Desestruturação de elemento para as coordenadas
+        let [a, b, c, d] = planeta.coordinates
+
+        list += (`
+        Nome: ${planeta.name}
+        Coordenadas: (${a}, ${b}, ${c}, ${d})
+        Situação: ${planeta.situation}
+        Satélites: ${planeta.satellites.length}`)
+        //Agregando os satélites
+        planeta.satellites.forEach( satelite => {
+            list += (`\n            - ${satelite}`)
+        })
+    })
+    //Exibir texto
+    alert(list)
+};
+
+//Menu
+let perg:boolean = true
+while(perg){
+    
+    let option = Number(prompt(`
+    MENU
+    1 - Registrar um novo planeta
+    2 - Atualizar situação do planeta
+    3 - Adicionar um satélite ao planeta
+    4 - Remover um satélite do planeta
+    5 - Lista todos os planetas
+    6 - Sair
+    `))
+
+    switch(option){
+        case 1:
+            primeiraF()
+            break
+        case 2:
+            segundaF()
+            break
+        case 3:
+            terceiraF()
+            break
+        case 4:
+            quartaF()
+            break
+        case 5:
+            quintaF()
+            break
+        case 6:
+            perg = false
+            break
+        default:
+            alert("Digite uma opção válida.")
+            break
+    }
+};
